330b153ee60b94d6fa4a5bd679c0f26848d8ef77
CherokiDev <miguelangelmorato84@gmail.com>
2023-08-15 19:29:40 +0200

    Revert "test revert 2"
    This reverts commit 18fa44b5f0e62e1af41d7004beb7c7f8a21a20f8.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,6 @@ import { LinkContainer } from 'react-router-bootstrap';
 const Hero = () => {
     return (
         <div className=' py-5'>
-            test 2
             <Container className='d-flex justify-content-center'>
                 <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
                     <h1 className='text-center mb-4'>
